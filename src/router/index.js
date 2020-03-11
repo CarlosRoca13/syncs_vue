8f9b3d31fdbc86da84e6bb4b31c1369462c1ed83
CarlosRoca13 <al361876@uji.es>
2020-03-11 18:49:44 +0100

    Estructura

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
+import Upload from '../components/Upload.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/upload',
+    name: 'Upload',
+    component: Upload
   }
 ]
 
