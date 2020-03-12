01c520f096578ec1915ace5ae343906aed86fc6b
Mackyplz <miguelligeron@gmail.com>
2020-03-12 16:55:44 +0100

    Toolbar done, la lateral en proces

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,6 @@
 <template>
   <v-app>
-    <v-btn @click="peticionPost">
-        click
-    </v-btn>
+    
   </v-app>
 </template>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav>
+    <v-app-bar absolute dark app>
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Syncs</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <div class="searchbar">
+        <v-text-field v-model="message4" label="Search..." solo clearable style="height:24px" background-color="grey darken-3"></v-text-field>
+      </div>
+
+      <v-spacer></v-spacer>
+      <v-btn text>Login</v-btn>
+
+      <v-btn outlined>Sign Up</v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer" dark>
+      <p>test</p>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
+</script>
+
+<style>
+.searchbar {
+  margin-bottom: 24px;
+  width: 300px;
+}
+</style>
