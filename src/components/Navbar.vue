90d213c4938c1f7295d5502e87f219aa2bffc96a
Mackyplz <miguelligeron@gmail.com>
2020-04-08 09:54:53 +0200

    Cambios en el navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,18 +8,47 @@
       <v-spacer></v-spacer>
 
       <div class="searchbar">
-        <SearchBar/>
+        <SearchBar />
       </div>
 
       <v-spacer></v-spacer>
-      <v-btn text v-on:click="login()">Login</v-btn>
+      <div v-if="userLogged==false">
+        <v-btn text v-on:click="login()">Login</v-btn>
+        <v-btn text v-on:click="register()" outlined>Sign Up</v-btn>
+      </div>
+      <div v-else>
+        <v-menu offset-y class="profileMenu">
+          <template v-slot:activator="{ on }">
+            <div class="loggedUsername" v-on="on">
+              <span class="loggedUsername">{{username}}</span>
+              <v-avatar color="#bf4222" size="40">
+                <span class="white--text headline">
+                  <v-icon>account_circle</v-icon>
+                </span>
+              </v-avatar>
+            </div>
+          </template>
 
-      <v-btn text v-on:click="register()" outlined>Sign Up</v-btn>
+          <v-list class="profileMenuContent">
+            <v-list-item v-for="item in menuItems" :key="item.title" @click="d">
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
     </v-app-bar>
 
     <v-navigation-drawer dark app v-model="drawer" style="background-color: #183a37">
       <div class="logo">
-      <a href="/"><v-img src="../assets/syncs.png" width="200"></v-img></a>
+        <a href="/">
+          <v-img src="../assets/syncs.png" width="200"></v-img>
+        </a>
       </div>
 
       <v-list class="menu-lateral">
@@ -38,27 +67,44 @@
 </template>
 
 <script>
-import SearchBar from './searchBar'
+import SearchBar from "./searchBar";
 export default {
-   components: {
+  components: {
     SearchBar
   },
   data() {
     return {
+      username: null,
+      userLogged: null,
       drawer: false,
+      menuItems: [
+        { title: "Edit Profile", icon: "create", route: "" },
+        { title: "My Sheets", icon: "music_note", route: "" },
+        { title: "Logout", icon: "exit_to_app", route: "" }
+      ],
       items: [
-        { title: "Artists", icon: "people_alt", route:"/artists"},
-        { title: "Sheets", icon: "music_note", route:"/sheets" },
-        { title: "Playlists", icon: "queue_music", route:"/playlists" }
+        { title: "Artists", icon: "people_alt", route: "/artists" },
+        { title: "Sheets", icon: "music_note", route: "/sheets" },
+        { title: "Playlists", icon: "queue_music", route: "/playlists" }
       ]
     };
   },
-  methods:{
-    login(){
+  methods: {
+    login() {
       this.$router.replace({ name: "Login" });
     },
-    register(){
+    register() {
       this.$router.replace({ name: "Register" });
+    }
+  },
+  mounted() {
+    let activeUser = localStorage.getItem("activeUser");
+    const item = JSON.parse(activeUser);
+    if (item == null) {
+      this.userLogged = false;
+    } else {
+      this.username = item.data.username;
+      console.log(item);
     }
   }
 };
@@ -69,12 +115,20 @@ export default {
   margin-bottom: 24px;
   width: 300px;
 }
-.logo{
+.logo {
   margin-left: 20px;
   margin-top: 15px;
 }
-.menu-lateral{
+.menu-lateral {
   margin-top: 30px;
 }
-
+.profileMenu {
+  padding-top: 10px;
+}
+.profileMenuContent {
+  padding-right: 10px;
+}
+.loggedUsername {
+  padding-right: 20px;
+}
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,215 +1,223 @@
 <template>
-    <div>
-        <div class="container-all">
+  <div class="main">
+    <div class="container-all">
+      <div class="ctn-form">
+        <img src="../img/logo.png" alt="Syncs" class="logo" />
+        <h1 class="title">Sign in</h1>
+        <ValidationObserver v-slot="{ handleSubmit }">
+          <form v-on:submit.prevent="handleSubmit(login)">
+            <ValidationProvider name="username" rules="required" v-slot="{ errors }">
+              <label for="username">Username</label>
+              <input type="text" v-model="input.username" name="username" />
+              <span>{{ errors[0] }}</span>
+            </ValidationProvider>
+            <ValidationProvider name="password" rules="required" v-slot="{ errors }">
+              <label for="password">Password</label>
+              <input type="password" v-model="input.password" name="password" />
+              <span>{{ errors[0] }}</span>
+            </ValidationProvider>
 
-            <div class="ctn-form">
-                <img src="../img/logo.png" alt="Syncs" class="logo">
-                <h1 class="title">Sign in</h1>
-				<ValidationObserver v-slot="{ handleSubmit }">
-					<form v-on:submit.prevent="handleSubmit(login)">
-
-						<ValidationProvider name="username" rules="required" v-slot="{ errors }">
-							<label for="username">Username</label>
-							<input type="text" v-model="input.username" name="username">
-							<span>{{ errors[0] }}</span>
-						</ValidationProvider>
-						<ValidationProvider name="password" rules="required" v-slot="{ errors }">
-							<label for="password">Password</label>
-							<input type="password" v-model="input.password" name="password">
-							<span>{{ errors[0] }}</span>
-						</ValidationProvider>
-						
-						<button type="submit">Login</button>
-						
-					</form>
-				</ValidationObserver>
-                <span class="text_footer">Not a member yet?
-                    <a href="">Sign up</a>
-                </span>
-            </div>
-            <div class="ctn-text">
-                <div class="capa"></div>
-                <h1 class="title-description">Title</h1>
-                <p class="text-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim voluptate, quidem omnis mollitia aliquam recusandae voluptatum architecto quaerat commodi laudantium et, reiciendis nobis non quae. Qui delectus magni cupiditate soluta.</p>
-            </div>
-        </div>
+            <button type="submit">Login</button>
+          </form>
+        </ValidationObserver>
+        <span class="text_footer">
+          Not a member yet?
+          <a href>Sign up</a>
+        </span>
+      </div>
+      <div class="ctn-text">
+        <div class="capa"></div>
+        <h1 class="title-description">Title</h1>
+        <p
+          class="text-description"
+        >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim voluptate, quidem omnis mollitia aliquam recusandae voluptatum architecto quaerat commodi laudantium et, reiciendis nobis non quae. Qui delectus magni cupiditate soluta.</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Login",
-        data() {
-            return {
-                input: {
-                    username: "",
-                    password: ""
-				},
-				submitted: false
-			}
-		},
-        methods: {
-            login: async function login() {
-				let logSucc = false;
-				const users = await this.$http.get("http://localhost:8000/api/clients");
-				for(const {username, password} of users.data.data){
-					if(username === this.input.username && password === this.input.password){
-						let activeUser = {
-							'username': this.input.username,
-							'token': Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-						}
-						localStorage.setItem("activeUser", JSON.stringify(activeUser));
-						this.$router.replace({ name: "home" });
-						logSucc = true;
-						break;
-					}
-				}
-				if(!logSucc){
-					alert("Wrong username or password")
-				}
-            }
+export default {
+  name: "Login",
+  data() {
+    return {
+      input: {
+        username: "",
+        password: ""
+      },
+      submitted: false
+    };
+  },
+  methods: {
+    login: async function login() {
+      let logSucc = false;
+      const users = await this.$http.get("http://localhost:8000/api/clients");
+      for (const { username, password } of users.data.data) {
+        if (
+          username === this.input.username &&
+          password === this.input.password
+        ) {
+          let activeUser = {
+            username: this.input.username,
+            token:
+              Math.random()
+                .toString(36)
+                .substring(2, 15) +
+              Math.random()
+                .toString(36)
+                .substring(2, 15)
+          };
+          localStorage.setItem("activeUser", JSON.stringify(activeUser));
+          this.$router.replace({ name: "home" });
+          logSucc = true;
+          break;
         }
+      }
+      if (!logSucc) {
+        alert("Wrong username or password");
+      }
     }
+  }
+};
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
 
-*{
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-	font-family: 'Roboto', sans-serif;
-	text-decoration: none;
+* {
+  box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
+  text-decoration: none;
 }
 
-.container-all{
-	width: 100%;
-	max-width: 1000px;
-	margin: auto;
-	margin-top: 50px;
-	display: flex;
-	border-radius: 20px;
-	overflow: hidden;
-	padding: 20px;
-	background: linear-gradient(90deg, #BF4222,
-	#183A37);
+.main{
+  background: linear-gradient(90deg, #bf4222, #183a37);
+}
+.container-all {
+  width: 100%;
+  max-width: 1000px;
+  margin: auto;
+  margin-top: 50px;
+  display: flex;
+  border-radius: 20px;
+  overflow: hidden;
+  padding: 20px;
 }
 
 /* LADO IZQUIERDO */
 
-.ctn-form{
-	width: 80%;
-	padding: 40px;
-	background: #f7f7f7;
+.ctn-form {
+  width: 80%;
+  padding: 40px;
+  background: #f7f7f7;
 }
 
-.logo{
-	width: 150px;
-	display: block;
-	margin: auto;
+.logo {
+  width: 150px;
+  display: block;
+  margin: auto;
 }
 
-.title{
-	text-align: center;
-	margin-top: 20px;
-	font-weight: 400;
-	color: #183A37;
+.title {
+  text-align: center;
+  margin-top: 20px;
+  font-weight: 400;
+  color: #183a37;
 }
 
-label{
-	display: block;
-	margin-top: 30px;
-	font-size: 20px;
-	font-weight: 300;
-	color: #7a7a7a;
+label {
+  display: block;
+  margin-top: 30px;
+  font-size: 20px;
+  font-weight: 300;
+  color: #7a7a7a;
 }
 
 input[type="text"],
-input[type="password"]{
-	width: 100%;
-	height: 30px;
-	background: rgba(0,0,0,0);
-	border: 0px;
-	outline: 0px;
-	border-bottom: 1px solid rgba(0,0,0,0.12);
-	color: #183A37;
-	font-size: 16px;
+input[type="password"] {
+  width: 100%;
+  height: 30px;
+  background: rgba(0, 0, 0, 0);
+  border: 0px;
+  outline: 0px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+  color: #183a37;
+  font-size: 16px;
 }
 
-button[type="submit"]{
-	width: 100%;
-	height: 50px;
-	margin-top: 60px;
-	margin-bottom: 30px;
-	color: white;
-	border: 0px;
-	background-color: #BF4222;
-	font-weight: 300;
-	cursor: pointer;
-	font-size: 18px;
+button[type="submit"] {
+  width: 100%;
+  height: 50px;
+  margin-top: 60px;
+  margin-bottom: 30px;
+  color: white;
+  border: 0px;
+  background-color: #bf4222;
+  font-weight: 300;
+  cursor: pointer;
+  font-size: 18px;
 }
 
-.text-footer{
-	display: block;
-	margin-top: 100px;
-	text-align: center;
-	color: #7a7a7a;
-	font-weight: 300;
+.text-footer {
+  display: block;
+  margin-top: 100px;
+  text-align: center;
+  color: #7a7a7a;
+  font-weight: 300;
 }
 
-.text-footer a{
-	color: #029EE1;
-	font-weight: 500;
+.text-footer a {
+  color: #029ee1;
+  font-weight: 500;
 }
 
 /* LADO DERECHO */
 
-.ctn-text{
-	width: 100%;
-	background-image: url("../img/imagen.jpg");
-	background-position: center;
-	background-size: cover;
-	padding: 40px;
-	position: relative;
+.ctn-text {
+  width: 100%;
+  background-image: url("../img/imagen.jpg");
+  background-position: center;
+  background-size: cover;
+  padding: 40px;
+  position: relative;
 }
 
-.capa{
-	width: 100%;
-	height: 100%;
-	position: absolute;
-	top: 0;
-	left: 0;
-	background-color: #183A37;
-	opacity: 0.5;
+.capa {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #183a37;
+  opacity: 0.5;
 }
 
-.title-description{
-	position: relative;
-	top: 80px;
-	color: white;
-	font-weight: 300;
-	font-size: 40px;
+.title-description {
+  position: relative;
+  top: 80px;
+  color: white;
+  font-weight: 300;
+  font-size: 40px;
 }
 
-.text-description{
-	position: relative;
-	top: 110px;
-	color: white;
-	font-size: 18px;
-	font-weight: 200;
+.text-description {
+  position: relative;
+  top: 110px;
+  color: white;
+  font-size: 18px;
+  font-weight: 200;
 }
 
 /* RESPONSIVE */
 
-@media screen and (max-width: 800px){
-	.ctn-text{
-		display: none;
-	}
-	.ctn-form{
-		margin: auto;
-		width: 100%;
-		background: white;
-	}
+@media screen and (max-width: 800px) {
+  .ctn-text {
+    display: none;
+  }
+  .ctn-form {
+    margin: auto;
+    width: 100%;
+    background: white;
+  }
 }
 </style>
