0aa52faf968c07e21c257a9aebaf435c94417b1a
JaviGR7 <al361872@uji.es>
2020-04-21 16:12:14 +0200

    Botó de seguiment

---
+++ src/components/FollowButton.vue
@@ -0,0 +1,64 @@
+<template>
+    <v-btn  @click="follow">
+        {{nombre}}
+    </v-btn>
+</template>
+
+<script>
+    export default {
+        data(){
+          return{
+              estado: false,
+              nombre: 'Seguir',
+              seguidores: []
+          }
+        },
+        props: {
+            user_id: Number,
+            follower_id: String,
+        },
+        methods: {
+            follow(){
+                if (this.estado){
+                    this.$http.delete('/api/follows/'+this.user_id + '/' + this.follower_id)
+                    this.estado=false
+                    this.nombre='Seguir'
+
+                }
+                else{
+                    this.$http.post('/api/follows',{
+                        user_id: this.user_id,
+                        follower_id: this.follower_id
+                    })
+                    this.nombre = 'Siguiendo'
+                    this.estado=true
+                }
+
+
+            },
+            checkFollow(){
+                    this.estado = this.seguidores.includes(this.follower_id)
+                    if (this.estado === true ){
+                        this.nombre = 'Siguiendo'
+                    }
+                    else{
+                        this.nombre='Seguir'
+                    }
+            }
+
+        }
+        ,
+        mounted() {
+            this.$http.get('/api/follows/' + this.user_id )
+                .then((result) => {
+                    this.seguidores = result.data
+                    this.checkFollow()
+                });
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
