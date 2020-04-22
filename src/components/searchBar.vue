cffbb5ce451bd0a71f5da8bbf8a67f4679abfe64
Rgnar98 <al361915@uji.es>
2020-04-22 16:33:31 +0200

    Prueba Search fallo

--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -2,7 +2,8 @@
   <div>
     <v-autocomplete
       v-model="queryTerm"
-      :items="names"
+      :items="entries"
+      :item-text="i => i.name"
       :search-input.sync="search"
       placeholder="Search..."
       solo
@@ -15,8 +16,8 @@
       @keyup="loadEntries"
       return-object
     >
-      <div slot="item" slot-scope="data" v-if="queryTerm" @click="changeView(data)">{{ data.item }}
-        <span class="artistSong" v-if="artists[getArtist(data)]!=null">by {{artists[getArtist(data)]}}</span>
+      <div slot="item" slot-scope="data" v-if="queryTerm" @click="changeView(data.item)">{{ data.item.name }}
+        <span class="artistSong" v-if="data.item.artist !=null">by {{data.item.artist}}</span>
       </div>
     </v-autocomplete>
   </div>
@@ -28,8 +29,6 @@ export default {
     return {
       newTag: "",
       entries: [],
-      names: [],
-      artists: [],
       queryTerm: "",
       isLoading: false,
     };
@@ -52,61 +51,63 @@ export default {
     async loadEntries() {
       if (this.isLoading) return
       this.entries = [];
-      this.names = [];
-      this.artists = [];
 
       if (this.queryTerm) {
         this.isLoading = true
-        this.$http
-          .get("/api/search", { params: { name: this.queryTerm } })
+        await this.$http
+          .get("/api/search/artist/"+this.queryTerm)
           .then(async response => {
-            this.names.push({ header: "Artists" });
+            this.entries.push({ header: "Artists" });
             for (let item in response.data) {
               if (response.data[item].client) {
+                console.log(response.data);
                 this.entries.push({ name: response.data[item].client, id: response.data[item].clientid, type: 'clients'});
-                this.names.push(response.data[0].client);
               }
             }
-            this.names.push({ header: "Songs" });
+            
+          }).finally(() => (this.isLoading = false));
+
+        await this.$http
+          .get("/api/search/song/"+this.queryTerm)
+          .then(async response => {  
+          this.entries.push({ header: "Songs" });
             for (let item in response.data) {
               if (response.data[item].sheet) {
+                console.log(response.data[item].sheet);
                 let res = await this.$http.get("/api/clientsong/"+response.data[item].sheetid)
+                console.log(res.data)
                 
                 this.entries.push({ name: response.data[item].sheet, id: response.data[item].sheetid, type: 'sheets', artist: res.data[0].client});
-                this.names.push(response.data[0].sheet);
-                this.artists.push(res.data[0].client);
                 
               }
             }
-            this.names.push({ header: "Playlists" });
+        }).finally(() => (this.isLoading = false));
+
+
+        await this.$http
+        .get("/api/search/playlist/"+this.queryTerm)
+        .then(async response => {  
+            this.entries.push({ header: "Playlists" });
             for (let item in response.data) {
               if (response.data[item].playlist) {
+                console.log(response.data);
                 let res = await this.$http.get("/api/clientplaylist/"+response.data[item].playlistid)
 
                 this.entries.push({ name: response.data[item].playlist, id: response.data[item].playlistid, type: 'playlists', artist: res.data[0].client});
-                this.names.push(response.data[0].playlist);
-                this.artists.push(res.data[0].client);
 
               }
             }
           }).finally(() => (this.isLoading = false));
+
       }
+      
     },
 
 
   changeView(data){
     //window.location.replace=data.item.type+'/'+data.item.id
-    var type = this.entries.find(x => x.name === data.item).type;
-    var id = this.entries.find(x => x.name === data.item).id;
-    window.location.href = 'http://localhost:8080/'+type+'/'+id;
+    window.location.href = 'http://localhost:8080/'+data.type+'/'+data.id;
   
-  },
-
-  
-
-  getArtist(data){
-    var artist = this.entries.findIndex(x => x.name === data.item);
-    return artist;
   },
 
   }
