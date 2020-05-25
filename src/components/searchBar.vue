342f18507ed6eb23a26298071c2fdc1ba99b6fcf
Rgnar98 <al361915@uji.es>
2020-05-25 15:27:31 +0200

    searchbar estilitzat

--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     
-    <v-text-field  v-on:keyup.enter="search()" label="Search..." clearable
+    <v-text-field  v-on:keyup.enter="search()" label="Search..." clearable solo append-icon="search"
     style="height:24px" v-model="queryTerm" background-color="grey darken-3">
     </v-text-field>
     
@@ -20,72 +20,15 @@ export default {
   },
 
   methods: {
-    /* async loadEntries() {
-      if (this.isLoading) 
-        return
-
-      this.entries = [];
-
-      if (this.queryTerm) {
-        
-        this.isLoading = true
-        await this.$http
-          .get("/api/search/artist/"+this.queryTerm)
-          .then(async response => {
-            this.entries.push({ header: "Artists" });
-            for (let item in response.data) {
-              if (response.data[item].client) {
-                console.log(response.data);
-                this.entries.push({ name: response.data[item].client, id: response.data[item].clientid, type: 'clients'});
-              }
-            }
-            
-          }).finally(() => (this.isLoading = false));
-
-        await this.$http
-          .get("/api/search/song/"+this.queryTerm)
-          .then(async response => {  
-            this.entries.push({ header: "Songs" });
-            for (let item in response.data) {
-              if (response.data[item].sheet) {
-                console.log("song:",response.data[item].sheet);
-                let res = await this.$http.get("/api/clientsong/"+response.data[item].sheetid)
-                
-                this.entries.push({ name: response.data[item].sheet, id: response.data[item].sheetid, type: 'sheets', artist: res.data[0].client});
-                
-              }
-            }
-        }).finally(() => (this.isLoading = false));
-
-
-        await this.$http
-        .get("/api/search/playlist/"+this.queryTerm)
-        .then(async response => {  
-            this.entries.push({ header: "Playlists" });
-            for (let item in response.data) {
-              if (response.data[item].playlist) {
-                console.log(response.data);
-                let res = await this.$http.get("/api/clientplaylist/"+response.data[item].playlistid)
-
-                this.entries.push({ name: response.data[item].playlist, id: response.data[item].playlistid, type: 'playlists', artist: res.data[0].client});
-
-              }
-            }
-          }).finally(() => (this.isLoading = false));
-
-
-
-      
-      this.isLoading = false;
-
-      }
-      
-    }, */
-
 
     search(){
-      this.$router.push({ name: "SearchData", query: {q: this.queryTerm}});
-      this.$router.go({ name: "SearchData", query: {q: this.queryTerm}});
+      if(window.location.href.includes('/search')){ //cambiar por ajax
+        this.$router.push({ name: "SearchData", query: {q: this.queryTerm}});
+        this.$router.go({ name: "SearchData", query: {q: this.queryTerm}});
+      }else{
+        this.$router.push({ name: "SearchData", query: {q: this.queryTerm}});
+      }
+      
       //this.$emit("inputData", this.queryTerm);
       //this.queryTerm = "";
 
